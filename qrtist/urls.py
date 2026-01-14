b3744e02f8b294f68bee6d3445249f4daf728508
Maruf Hossain <maruf.bshs@gmail.com>
2026-01-15 02:50:10 +0600

    fixed the media file serving issue

--- qrtist/urls.py
+++ qrtist/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.static import serve
 from generator.views import api_docs_view, home_view, qr_history_view, stats_view
 from django.conf import settings
 from django.conf.urls.static import static
@@ -16,6 +16,16 @@ urlpatterns = [
     path('docs/', api_docs_view, name='api_docs'),
 
 ]
+
+# Serve media files in both development and production
 if settings.DEBUG:
+    # Development: use static() helper
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # Production: use serve() view with proper restrictions
+    urlpatterns += [
+        path('media/<path:path>', serve, {
+            'document_root': settings.MEDIA_ROOT,
+            'show_indexes': False,
+        }),
+    ]
